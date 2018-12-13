41a149f7bce77259435cfea7d82ba252241fdf54
Roman Zinaliev <roman.zinaliev@genesys.com>
2018-12-13 12:09:53 +0300

    transfer service javadoc updated

--- src/main/java/org/zinaliev/transfermanager/service/TransferService.java
+++ src/main/java/org/zinaliev/transfermanager/service/TransferService.java
@@ -5,6 +5,7 @@ public interface TransferService {
     /**
      * Transfer money between wallets
      *
+     * @throws org.zinaliev.transfermanager.exception.NotFoundException in case source or target wallet is missing
      * @throws org.zinaliev.transfermanager.exception.TransferException operation can not be performed with the given input data
      */
     void transfer(String sourceWalletId, String targetWalletId, double moneyAmount);
